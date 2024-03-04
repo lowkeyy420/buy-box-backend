b095165598c5fbf813bcbba5de7336baa9de3640
lowkeyy420 <josafatandersonyo@gmail.com>
2024-03-04 13:01:25 +0700

    feat: Added user response when authenticating token

--- src/backend/express_canister/service/user_token.service.ts
+++ src/backend/express_canister/service/user_token.service.ts
@@ -1,23 +1,31 @@
 import { Request } from "express";
-import { tokenStorage } from "../db/data";
+import { tokenStorage, usersStorage } from "../db/data";
 
 export function authenticateToken(req: Request<any, any, any>, res: any) {
-    const tokenRequest = req.headers.authorization?.split(' ')[1];
+    const tokenRequest = req.headers.authorization?.split(" ")[1];
 
     if (!tokenRequest) {
-        return res.status(401).send('no token');
+        return res.status(401).send("no token");
     }
 
     const tokenOpt = tokenStorage.get(tokenRequest);
 
     if ("None" in tokenOpt) {
-        return res.status(401).send('token not in storage');
+        return res.status(401).send("token not in storage");
     }
 
     if (tokenOpt.Some.expiration < Date.now() / 1000) {
-        tokenStorage.remove(tokenRequest)
-        return res.status(401).send('token expired');
+        tokenStorage.remove(tokenRequest);
+        return res.status(401).send("token expired");
     }
 
-    return res.json(tokenOpt.Some)
+    const userOpt = usersStorage.get(tokenOpt.Some.user_id);
+    if ("None" in userOpt) {
+        return res.status(500).send("user_id is invalid");
+    }
+
+    return res.json({
+        token: tokenOpt.Some,
+        user: { ...userOpt.Some },
+    });
 }

01478d255f04cdea1cf198321102acb37c577e35
lowkeyy420 <josafatandersonyo@gmail.com>
2024-03-05 08:05:54 +0700

    refactor: Changed token authentication into middleware

---
+++ src/backend/express_canister/routes/middleware.ts
@@ -0,0 +1,36 @@
+import { Request } from "express";
+import { tokenStorage, usersStorage } from "../db/data";
+
+export function checkLoggedIn(req: Request<any, any, any>, res: any, next: any) {
+    const tokenRequest = req.headers.authorization?.split(" ")[1];
+
+    if (!tokenRequest) {
+        return res.status(400).send("no token");
+    }
+
+    const tokenOpt = tokenStorage.get(tokenRequest);
+
+    if ("None" in tokenOpt) {
+        return res.status(401).send("token not in storage");
+    }
+
+    if (tokenOpt.Some.expiration < Date.now() / 1000) {
+        const deletedTokenOpt = tokenStorage.remove(tokenOpt.Some.token);
+
+        if ("None" in deletedTokenOpt) {
+            return res.status(500).send("token not removed");
+        }
+        return res.status(401).send("token expired ");
+    }
+
+    const userOpt = usersStorage.get(tokenOpt.Some.user_id);
+    if ("None" in userOpt) {
+        return res.status(500).send("user_id is invalid");
+    }
+
+    (req as any).user = userOpt.Some;
+    (req as any).token = tokenOpt.Some;
+
+
+    next();
+};
--- src/backend/express_canister/service/user_token.service.ts
+++ src/backend/express_canister/service/user_token.service.ts
@@ -3,35 +3,10 @@ import { tokenStorage, usersStorage } from "../db/data";
 import UserTokenResponseDTO from "../dto/response/user_token.authenticate.dto";
 
 export function authenticateToken(req: Request<any, any, any>, res: any) {
-    const tokenRequest = req.headers.authorization?.split(" ")[1];
-
-    if (!tokenRequest) {
-        return res.status(400).send("no token");
-    }
-
-    const tokenOpt = tokenStorage.get(tokenRequest);
-
-    if ("None" in tokenOpt) {
-        return res.status(401).send("token not in storage");
-    }
-
-    if (tokenOpt.Some.expiration < Date.now() / 1000) {
-        const deletedTokenOpt = tokenStorage.remove(tokenOpt.Some.token);
-
-        if ("None" in deletedTokenOpt) {
-            return res.status(500).send("token not removed");
-        }
-        return res.status(401).send("token expired ");
-    }
-
-    const userOpt = usersStorage.get(tokenOpt.Some.user_id);
-    if ("None" in userOpt) {
-        return res.status(500).send("user_id is invalid");
-    }
 
     const response: UserTokenResponseDTO = {
-        token: tokenOpt.Some,
-        user: { ...userOpt.Some },
+        token: (req as any).token,
+        user: { ...(req as any).user },
     }
 
     return res.json(response);

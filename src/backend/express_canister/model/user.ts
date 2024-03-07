abb86d45e246afeb9ec9ccd2f38b84bab6de48b5
lowkeyy420 <josafatandersonyo@gmail.com>
2024-03-07 23:45:02 +0700

    feat: Added balance

--- src/backend/express_canister/model/user.ts
+++ src/backend/express_canister/model/user.ts
@@ -5,19 +5,22 @@ class User {
   full_name: string;
   password: string;
   is_store: boolean;
+  balance: number;
 
   constructor(id: string,
     username: string,
     email: string,
     full_name: string,
     password: string,
-    is_store: boolean) {
+    is_store: boolean,
+    balance: number) {
     this.id = id;
     this.username = username;
     this.email = email;
     this.full_name = full_name;
     this.password = password;
     this.is_store = is_store;
+    this.balance = balance;
   }
 
 };
